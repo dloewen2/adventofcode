dbec3190f6d13ed8fcfce82a3d5d3f7621d6730d
Dominik Löwen <d.loewen@cloudpirates.io>
2023-12-04 09:31:17 +0100

    Add Result Day01

---
+++ Day01/script.py
@@ -0,0 +1,15 @@
+import csv
+import re
+
+result = 0
+
+with open('input.csv', newline='') as csvfile:
+  reader = csv.reader(csvfile)
+  for row in reader:
+    regexMatches = re.findall(r"\d", row[0])
+    if len(regexMatches) > 1:
+      result += int(int(regexMatches[0] + regexMatches[len(regexMatches) - 1]))
+    else:
+      result += int(regexMatches[0] + regexMatches[0])
+    
+print(result)
